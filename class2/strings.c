3366ae3ca74f532df9165538c3233c1ff12f0ec9
student <student@localhost.localdomain>
2018-06-01 00:37:11 -0400

    Class 2 examples

--- class2/strings.c
+++ class2/strings.c
@@ -1,13 +1,16 @@
 #include<stdio.h>
 
+
+/* You may need to play around with the values to create the same problems on the last examples */
+/*  This code is very machine-specific */
 #define LEN 20
 
 int main() {
-  int ii;
+  int ii;  /* Variables must be declared at start of block in ANSI C */
   char buff1[LEN];
   char s1[] = "Hello";    /* s1 = {'H', 'e', 'l', 'l', 'o', '\0'} */
   char s2[5] = "world";   /* s2 = {'w', 'o', 'r', 'l', 'd'} */
-  char s3[] = {'S', 't', 'r', 'i', 'n', 'g', '\0'};
+  char s3[] = {'S', 't', 'r', 'i', 'n', 'g', '\0'};  /* s3 = "String" */
   char buff2[LEN];
 
   for(ii=0;ii<LEN;ii++) {
@@ -22,18 +25,22 @@ int main() {
   printf("s3: %s\n", s3);
   printf("buff2: %s\n", buff2);
 
-  for (ii=0;ii<15;ii++) {
+  /* This might be a problem!  C does not do array bounds checking. */
+  printf("\n");
+  for (ii=0;ii<20;ii++) {
     printf (" s2[%d]: %c (%d)\n", ii, s2[ii], s2[ii]);
   }
 
-  /* This might be a problem! */
-  /*
-  s2[10] = '*';
+  /* This might also be a problem! */
+  printf("\n");
+  s2[5] = '*';
   printf("s2: %s\n", s2);
-  s3[6] = '*';
+  s2[17] = '*';
   printf("s2: %s\n", s2);
-  */
+  printf("s1: %s\n", s1);
 
+  /* More trouble */
+  printf("\n");
   s1[3] = 'Z';
   s1[5] = 43;
   s1[6] = 'X';
