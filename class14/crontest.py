d9912eef8d2aa8b13b41d05a0512603f7f44e0f2
Wade Huber <wadehuber@gmail.com>
2018-07-12 23:41:02 -0400

    Class 14 updates

--- class14/crontest.py
+++ class14/crontest.py
@@ -2,8 +2,10 @@
 
 import time
 
-outfile = open("/home/csc240/220/class13/outfile.txt", 'a')
+print("Starting cron job")
+outfile = open("/home/student/220/csc220summer18/class14/outfile.txt", 'a')
 
 outstr = time.strftime("%H:%M:%S") + "\n"
 time.sleep(5)
 outfile.write(outstr)
+print("Finishing cron job")
--- class14/exceptions.cpp
+++ class14/exceptions.cpp
@@ -4,35 +4,45 @@ int quotient(int top, int bot) {
   return top / bot;
 }
 
-int shift(int a, int b) {
+
+int product(int a, int b) {
   return a * b;
 }
 
+
 void doStuff(int x, int y) {
-    try {
-        if ( (x==13) || (y==13) ) {
-            throw 13;
-        }
-        std::cout << "  " << x << " * " << y << " = " << shift(x,y) << std::endl;
-        if ( (y == 0) ) {
-            throw 0;
-        }
-        std::cout << "  " << x << " / " << y << " = " << quotient(x,y) << std::endl;
+  try {
+    std::cout << "  " << x << " * " << y << " = " << product(x, y) << std::endl;
+    
+    if ( (x==13) || (y==13) ) {
+      throw 13;
     }
-    catch(int e) {
-        if (e == 0)
-            std::cout << "Warning: attempt to devide by 0!" << std::endl;
-        else 
-            std::cout << "Unlucky number !" << e << "!" << std::endl;
+    if ( ( y == 0) ) {
+      throw 0;
     }
+    std::cout << "  " << x << " / " << y << " = " << quotient(x, y) << std::endl;
+  }
+
+  catch(int e) {
+    if (e == 0) {
+      std::cerr << "Error: attempt to divide by 0!" << std::endl;
+    }
+    else if (e == 13) {
+      std::cerr << "Error: unlucky number " << e << "!!!" << std::endl;
+    }
+    else {
+      std::cerr << "Error: something unexpected happened!" << std::endl;
+    }
+  }
 }
+
 
 int main() {
   std::cout << "doStuff(2,3):" << std::endl;
-  doStuff(2,3); 
+  doStuff(2,3);
   std::cout << std::endl << "doStuff(2,0):" << std::endl;
-  doStuff(2,0); 
-  std::cout << std::endl << "doStuff(26,13):" << std::endl;
-  doStuff(13,2); 
+  doStuff(2,0);
+  std::cout << std::endl << "doStuff(2,13):" << std::endl;
+  doStuff(2,13);
   return 0;
 }
